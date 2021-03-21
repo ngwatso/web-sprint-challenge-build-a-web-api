d5d06eec56619d53ca456f4c3e342a9e2ac8a259
ngwatso <ngwatson79@yahoo.com>
2021-03-21 10:01:40 -0400

    final

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const server = require('./api/server');
 
-const port = process.env.PORT;
-// const port = 5050;
+// const port = process.env.PORT;
+const port = 5050;
 server.listen(port, () => {
 	console.log(`\n *** Server running on http://localhost:${port} *** \n`);
 });
